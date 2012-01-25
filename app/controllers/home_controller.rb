a7fadf602d19dc21015aa7816cc776a81f0bf904
Live session user <ubuntu@ubuntu.(none)>
2012-01-25 22:28:42 +0000

    Config complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+	@phone_number = '6315551234'
+	@body = params.has_key?(:Body) ? params[:Body] : "NA"
+	@from = params.has_key?(:From) ? params[:From] : "NA"
+
   end
 end
