aca02761489a816bd7363b5614c616e06dcfb908
Live session user <ubuntu@ubuntu.(none)>
2012-01-25 18:58:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshToast::Application.config.secret_token = '9ffd8d8e71264c4ea44bff7e1be122b98f691da3acea7b4683c957953630daf73133ff12421042dbd3003c32b5573483d4e93b80dd2899de97c056f6dbd25831'
