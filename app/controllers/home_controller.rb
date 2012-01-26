bd635250f172f90a482cd7869dbc04e2b0dc751f
Timothy Imhof <timhof@timhof-HP-G70-Notebook-PC.(none)>
2012-01-25 21:24:16 -0500

    First commit from HP laptop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
+ require 'builder'
+
+
   def index
 	@phone_number = '6315551234'
 	@body = params.has_key?(:Body) ? params[:Body] : "NA"
 	@from = params.has_key?(:From) ? params[:From] : "NA"
 
+	puts twilio_response
   end
+
+  def twilio_response
+	xml = Builder::XmlMarkup.new( :indent => 2 )
+	xml.instruct! :xml, :encoding => "UTF-8"
+	xml.Response do |p|
+	  p.Sms "Got it"
+	end
+  end	
+
 end
